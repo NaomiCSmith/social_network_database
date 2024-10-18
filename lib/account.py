49343b0fdce5191c122c825d51d7f98cb64c6f6a
Naomi <naomismith97@yahoo.co.uk>
2024-10-18 12:12:41 +0100

    complete account class and tests

--- lib/account.py
+++ lib/account.py
@@ -1,4 +1,11 @@
-from lib.post import Post
-
 class Account():
-    pass
+    def __init__(self, id, username, email_address):
+        self.id = id
+        self.username = username
+        self.email_address = email_address
+    
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+    
+    def __repr__(self):
+        return f"Account({self.id}: {self.username}, {self.email_address})"
--- tests/test_account.py
+++ tests/test_account.py
@@ -1,3 +1,17 @@
-from lib.post import Post
 from lib.account import Account
 
+def test_account_constructs():
+    account = Account(1, "My username", "My email")
+    assert account.id == 1
+    assert account.username == "My username"
+    assert account.email_address == "My email"
+
+def test_account_are_equal():
+    account1 = Account(1, "Test username", "Test email")
+    account2 = Account(1, "Test username", "Test email")
+    assert account1 == account2
+
+
+def test_accounts_format_correctly():
+    account = Account(1, "Test username", "Test email")
+    assert str(account) == "Account(1: Test username, Test email)"
