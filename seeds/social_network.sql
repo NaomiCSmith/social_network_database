7dbe6f0b024c954134a20ed3b7440c4bb31d8fbf
Naomi <naomismith97@yahoo.co.uk>
2024-10-18 13:59:47 +0100

    corrected drop sequence commands

--- seeds/social_network.sql
+++ seeds/social_network.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS accounts;
-DROP TABLE IF EXISTS accounts_id_seq;
+DROP SEQUENCE IF EXISTS accounts_id_seq;
 DROP TABLE IF EXISTS posts;
-DROP TABLE IF EXISTS posts_id_seq;
+DROP SEQUENCE IF EXISTS posts_id_seq;
 
 CREATE SEQUENCE IF NOT EXISTS accounts_id_seq;
 CREATE TABLE accounts (
@@ -17,5 +17,13 @@ CREATE TABLE posts (
   content text,
   views int,
   account_id int,
-  constraint fk_cohort foreign key(account_id) references accounts(id) on delete cascade
+  constraint fk_account foreign key(account_id) references accounts(id) on delete cascade
 );
+
+INSERT INTO accounts (username, email_address) VALUES ('naomismith', 'naomismith@gmail.com');
+INSERT INTO accounts (username, email_address) VALUES ('bilbobaggins', 'bilbohatestravel@yahoo.co.uk');
+INSERT INTO accounts (username, email_address) VALUES ('mickeymouse12', 'mickeymickeymouse@hotmail.com');
+
+INSERT INTO posts (title, content, views, account_id) VALUES ('Naomis Diary', 'There is not much to say...', 1, 1);
+INSERT INTO posts (title, content, views, account_id) VALUES ('How to have the best second breakfast', 'Do not let anyone else in!', 5, 2);
+INSERT INTO posts (title, content, views, account_id) VALUES ('Plutos birthday', 'Hey everybody! It is his birthday today!', 2, 3);
