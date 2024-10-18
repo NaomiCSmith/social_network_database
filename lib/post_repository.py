61fc3dd93fa63180be4513f2461284dc03b60cb7
Naomi <naomismith97@yahoo.co.uk>
2024-10-18 15:09:32 +0100

    post repository class and tests

---
+++ lib/post_repository.py
@@ -0,0 +1,25 @@
+from lib.post import *
+
+class PostRepository():
+    def __init__(self, _connection):
+        self._connection = _connection
+    
+    def all(self):
+        rows = self._connection.execute('SELECT * FROM posts ORDER BY id ASC')
+        return [
+            Post(row["id"], row["title"], row["content"], row["views"], row["account_id"])
+            for row in rows]
+    
+    def find(self, post_id):
+        rows = self._connection.execute('SELECT * from posts WHERE id = %s', [post_id])
+        row = rows[0]
+        return Post(row["id"], row["title"], row["content"], row ["views"], row["account_id"])
+
+    def create(self, post):
+        self._connection.execute('INSERT INTO posts (title, content, views, account_id) VALUES (%s, %s, %s, %s)', [post.title, post.content, post.views, post.account_id])
+        return None
+
+    def delete(self, post_id):
+        self._connection.execute('DELETE FROM posts WHERE id = %s', [post_id])
+        return None
+    
---
+++ tests/test_post_repository.py
@@ -0,0 +1,39 @@
+from lib.post import *
+from lib.post_repository import *
+
+def test_all(db_connection):
+    db_connection.seed("seeds/social_network.sql")
+    repository = PostRepository(db_connection)
+    assert repository.all() == [
+        Post(1, "Naomis Diary", "There is not much to say...", 1, 1),
+        Post(2, "How to have the best second breakfast", "Do not let anyone else in!", 5, 2),
+        Post(3, "Plutos birthday", "Hey everybody! It is his birthday today!", 2, 3)
+    ]
+
+def test_find(db_connection):
+    db_connection.seed("seeds/social_network.sql")
+    repository = PostRepository(db_connection)
+    post = repository.find(1)
+    assert post == Post(1, "Naomis Diary", "There is not much to say...", 1, 1)
+
+def test_create(db_connection):
+    db_connection.seed("seeds/social_network.sql")
+    repository = PostRepository(db_connection)
+    repository.create(Post(None, "I'm tired", "I can't sleep", 3, 1))
+    result = repository.all()
+    assert result == [
+        Post(1, 'Naomis Diary', 'There is not much to say...', 1, 1),
+        Post(2, 'How to have the best second breakfast', 'Do not let anyone else in!', 5, 2),
+        Post(3, 'Plutos birthday', 'Hey everybody! It is his birthday today!', 2, 3),
+        Post(4, "I'm tired", "I can't sleep", 3, 1)
+    ]
+
+def test_delete(db_connection):
+    db_connection.seed("seeds/social_network.sql")
+    repository = PostRepository(db_connection)
+    repository.delete(1)
+    result = repository.all()
+    assert result == [
+        Post(2, 'How to have the best second breakfast', 'Do not let anyone else in!', 5, 2),
+        Post(3, 'Plutos birthday', 'Hey everybody! It is his birthday today!', 2, 3)
+    ]
